56e9b8f63baf2c3183334bd658148e367a176ead
paul <paulramey28@gmail.com>
2023-09-18 14:16:54 -0400

    Chapter 2.3 .py

--- Chapter 2 excercises.py
+++ Chapter 2 excercises.py
@@ -4,3 +4,14 @@
 name = input('What is your name?\n')
 print(f'hello, {name}')
 
+hours = 35
+rate = 2.75
+pay = hours * rate
+print(pay)
+
+width = 17
+height = 12.0
+width//2
+width/2.0
+height/3
+1 + 2 * 5
---
+++ Chapter 2.3 .py
@@ -0,0 +1,25 @@
+#!/bin/python3
+width = 17
+height = 12.0
+C = width//2
+print(C)
+
+remainder = width % 2
+print(remainder)
+
+width/2.0
+C = width/2.0
+print(C)
+
+height/3
+C = height/3
+print(C)
+
+1 + 2 * 5
+C = 1 + 2 * 5
+print(C)
+
+Temp = input('Enter a Celsius temp: ')
+Cel = float(Temp)
+Fahr = (9/5) * Cel + 32
+print(Fahr)
